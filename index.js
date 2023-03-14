3712d0ba4e0a3877e13d5a8fa977ce28f2d2a70b
Kennedy <kiplangatkennedy019@gmail.com>
2023-03-14 11:58:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code in this file!
+// const currentUser = 'Grace Hopper';
+const currentUser='Grace Hopper';
+// const welcomeMessage = 'Welcome to Flatbook,${currentUser}! ';
+// const welcomeMessage = currentUser;
+// const welcomeMessage = 'Welcome to Flatbook, currentUser';
+// typeof "const currentUser = 'Grace Hopper';";
+// "const currentUser = 'Grace Hopper';".length;
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+// const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// const shortGreeting = 'welcome';
+
+// const shortGreeting = 'Welcome, ';
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
